6ec1d7f8f52e8eb3667069947f97ccdc461fe4d4
Dinis-Xavier <202300148@estudantes.ips.pt>
2024-04-05 08:56:01 +0100

    Nível 1

---
+++ AirTransportation.java
@@ -0,0 +1,15 @@
+
+/**
+ * Escreva uma descrição da classe AirTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class AirTransportation extends Transport
+{
+    private static final double HONO = 4.0;
+    
+    private String name;
+    private int numberOfContainers;
+    
+}
---
+++ GroundTransportation.java
@@ -0,0 +1,12 @@
+
+/**
+ * Escreva uma descrição da classe GroundTransportation aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class GroundTransportation extends Transport
+{
+    private static final double HONO = 3.0;
+    private String licensePlate;
+}
